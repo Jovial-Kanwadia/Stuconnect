25795a2ea14b39c89b8b361391ccec6f944f90c0
Jovial <jovialkanwadia@gmail.com>
2024-08-26 00:52:14 +0530

    Error fixed in typescript build

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,6 @@
 // import dotenv from "dotenv"
-import connectDB from "../src/db/index.js"
-import { httpServer } from "../src/app.js"
+import connectDB from "./db/index.js"
+import { httpServer } from "./app.js"
 import dotenv from 'dotenv'
 dotenv.config({
     path: './.env'
