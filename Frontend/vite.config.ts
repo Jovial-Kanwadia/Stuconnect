c49c88a7994ebc3650cb8790d2be4947f7443ba3
Jovial <jovialkanwadia@gmail.com>
2024-08-28 16:51:16 +0530

    fixes

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,24 +1,17 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  server: {
+    port: process.env.PORT as unknown as number,
   },
-  build: {
-    outDir: 'dist', // Ensure this matches your Netlify publish directory
-    assetsDir: 'assets', // Directory for static assets
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`,
-      },
-    },
-  },
-  base: '/', // Ensure the base path is correctly set for Netlify
+  plugins: [vercel()],
 });
+
+// export default defineConfig(() => {
+//   return {
+//     define: {
+//       __APP_ENV__: process.env.VITE_BACKEND_URL,
+//     },
+//   };
+// });
