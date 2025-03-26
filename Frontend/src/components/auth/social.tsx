5e744537de35cb0078a4acd76115c0d0a590a617
Jovial-Kanwadia <143182327+Jovial-Kanwadia@users.noreply.github.com>
2025-03-27 03:17:59 +0530

    Update social.tsx

--- Frontend/src/components/auth/social.tsx
+++ Frontend/src/components/auth/social.tsx
@@ -1,11 +1,11 @@
-import { FcGoogle } from 'react-icons/fc'
-import { FaGithub } from 'react-icons/fa'
-import { Button } from '../ui/button'
+// import { FcGoogle } from 'react-icons/fc'
+// import { FaGithub } from 'react-icons/fa'
+// import { Button } from '../ui/button'
 
 export const Social = () => {
     return (
         <>
-        <div className="flex items-center w-full gap-x-2">
+{/*         <div className="flex items-center w-full gap-x-2">
         <Button
         size='lg'
         className='w-full bg-slate-200'
@@ -22,7 +22,7 @@ export const Social = () => {
         >
             <FaGithub className='h-5 w-5'/>
         </Button>
-        </div>
+        </div> */}
         </>
     )
 } 
